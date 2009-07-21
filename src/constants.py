2ddeff03d08bf71ba0a5d672d1a750dab08db3cf
Milton Mazzarri <milmazz@gmail.com>
2009-07-21 05:56:57 -0430

    Se agrega el directorio contenedor de las poesias. Se ajustan las rutas.

--- src/constants.py
+++ src/constants.py
@@ -38,8 +38,8 @@ associations_hover_filename = os.path.join(icons_folder, "gartoon", "association
 illustration_008 = os.path.join(data_folder, "backgrounds", "illustration_008.jpg")
 illustration_012 = os.path.join(data_folder, "backgrounds", "illustration_012.jpg")
 
-poetry = os.path.join(data_folder, "messages", "poetry.txt")
-poetry2 = os.path.join(data_folder, "messages", "poetry2.txt")
+poetry = os.path.join(data_folder, "poetries", "poetry.txt")
+poetry2 = os.path.join(data_folder, "poetries", "poetry2.txt")
 
 # sound buffer length
 mixer_buffersize = 3 * 1024
