830de2185811979965f360fd0fb2678747795fcf
Milton Mazzarri <milmazz@gmail.com>
2009-07-28 22:39:21 -0430

    Se integra la segunda actividad de sopa de letras en un solo fichero.

--- src/soups.py
+++ src/soups.py
@@ -225,3 +225,147 @@ class SoupActivity(Activity):
                     self.sprites.update(self.final_pos)
                     self.sprites.draw(self.screen)
                 pygame.display.update() #actualizar la pantalla
+
+
+class SoupActivity2(Activity):
+    def limpiar_letras_incorrectas(self, lista_coloreadas, palabras):
+        #usando los identificadores de las letras
+        #verificamos si son las correctas
+        #la variable check si es 0 no es correcta la palabra
+        self.check = 1
+        self.lista_ordenada = list()
+        #palabras correctas
+        #vista, gusto, tacto, olfato, oido
+        self.tamano_lista = len(lista_coloreadas)
+        #verificamos el tamano de la lista.
+        if self.tamano_lista >= 3 and self.tamano_lista <= 8:
+            #ordenos la lista por los id
+            aux = sorted(lista_coloreadas, self.comparar)
+            for i in range(self.tamano_lista):
+                self.lista_ordenada.append(aux[i].id)
+            #luz
+            if self.tamano_lista == 3 and self.lista_ordenada == [57,58,59]:
+                self.check = 2
+                palabras.update("luz")
+            #aire
+            elif self.tamano_lista == 4 and self.lista_ordenada == [8,9,10,11]:
+                self.check = 2
+                palabras.update("aire")
+            #agua
+            elif self.tamano_lista == 4 and self.lista_ordenada == [32,33,34,35]:
+                self.check = 2
+                palabras.update("agua")
+            #suelo
+            elif self.tamano_lista == 5 and self.lista_ordenada == [19,20,21,22,23]:
+                self.check = 2
+                palabras.update("gusto")
+            #plantas
+            elif self.tamano_lista == 7 and self.lista_ordenada == [1,2,3,4,5,6,7]:
+                self.check = 2
+                palabras.update("tacto")
+            #animales
+            elif self.tamano_lista == 8 and self.lista_ordenada == [40,41,42,43,44,45,46,47]:
+                self.check = 2
+                palabras.update("animales")
+            elif self.check != 2:
+                self.check = 0
+        else:
+            self.check = 0
+        if self.check == 0:
+            for item in lista_coloreadas:
+                item.color = 0
+
+    def comparar(self, a, b):
+        return cmp(int(a.id), int(b.id))
+
+    def __init__(self, screen):
+        Activity.__init__(self, screen)
+        
+    def setup_background(self):
+        self.background = pygame.image.load(constants.illustration_017).convert_alpha()
+        self.background = pygame.transform.scale(self.background, (800,600))
+
+    def setup(self):
+        if pygame.font:
+            font = pygame.font.Font(None, 40)
+            font.set_bold(True)
+            text = font.render("En busca de los cinco sentidos", 1, (0, 0, 0))
+            self.screen.blit(text, (10, 10))
+            
+            font = pygame.font.Font(None, 32)
+            font.set_bold(False)
+            text = font.render("Busca los cinco sentidos en la sopa de letras y", \
+            1,(0, 0, 0))
+            self.screen.blit(text, (50, 40))
+
+            text = font.render("marca cada palabra", 1,(0, 0, 0))
+            self.screen.blit(text, (50, 60))
+
+        self.lapiz = Lapiz()
+        self.puntalapiz = Puntalapiz()
+        #ocultamos el puntero del raton 
+        pygame.mouse.set_visible( False )
+        #grupos de sprite
+        self.sprites  = pygame.sprite.OrderedUpdates()
+        self.letras   = pygame.sprite.Group()
+        self.palabras = pygame.sprite.Group()
+        ### variables generales ###
+        self.mouse_down = 0 
+        self.pos_x = 215 #posicion inicial de las letras X
+        pos_y = 220 #posicion inicial de las letras Y
+        tam_x = 38  #tamano de los cuadros ancho
+        tam_y = 37  #tamano de los cuadros alto
+        self.lista = list() #lista de letras que estan coloreadas
+       
+        #agregar las letras sprite al grupo
+        letrasM = 'yplantasairenkefdalsueloouyraiumaguaqeioanimalesqntoaudoaluzatan'
+        for i in range(8):
+            for j in range(8):
+                self.letras.add([Letras(self.pos_x + tam_x * j, \
+                  pos_y + tam_y * i, letrasM[j + i * 8], j + i * 8)])
+           
+        self.palabras.add([Palabras(80, 370, 'vista'), Palabras(87, 400, 'gusto'), \
+          Palabras(88, 430, 'tacto'), Palabras(87, 460, 'oido'), \
+          Palabras(84, 490, 'olfato')])
+        
+        self.sprites.add([self.letras, self.lapiz, self.puntalapiz, self.palabras])
+        self.sprites.update((0,0))
+        self.sprites.draw(self.screen)
+
+        done = False
+
+    def handle_events(self):
+            for event in [ pygame.event.wait() ] + pygame.event.get():
+                if event.type == QUIT:
+                    self.quit = True
+                    return
+                elif event.type == KEYUP:
+                   if event.key == K_ESCAPE:
+                       self.quit = True
+                       return
+                elif event.type == MOUSEBUTTONDOWN:
+                    self.mouse_down = 1 #se presiono el boton del mouse
+                elif event.type == MOUSEMOTION:
+                    self.final_pos = pygame.mouse.get_pos() #posicion del puntero del mouse
+                    if self.mouse_down == 1 and self.final_pos[0] > self.pos_x:
+                        letra_coloreada = pygame.sprite.spritecollideany \
+                          (self.puntalapiz, self.letras)
+                        #si estamos sobre una letra y esta no esta coloreada
+                        #la coloreamos
+                        if letra_coloreada and letra_coloreada.color != 1:
+                            letra_coloreada.color = 1 #la coloreamos
+                            letra_coloreada.update()   #actualizamos la imagen de la letra
+                            self.lista.append(letra_coloreada)
+                    self.lapiz.update(self.final_pos)
+                    self.puntalapiz.update(self.final_pos)
+                    self.screen.blit(self.background, (0, 0)) #pintamos el fondo
+                    self.sprites.draw(self.screen) #dibujar los sprite
+                elif event.type == MOUSEBUTTONUP:
+                    self.mouse_down = 0 #se solto el boton del mouse
+                    self.limpiar_letras_incorrectas(self.lista, self.palabras)
+                    #limpiar la lista
+                    self.lista = list()
+                    self.screen.blit(self.background, (0, 0))
+                    self.sprites.update(self.final_pos)
+                    self.sprites.draw(self.screen)
+                pygame.display.update() #actualizar la pantalla
