40123039be579c126aa5ce9ae2a9e1aa7c2fbb95
Milton Mazzarri <milmazz@gmail.com>
2009-08-03 02:40:17 -0430

    Se implementan sprites en las actividades de lectura, se agrega el boton de regreso al menú principal.

--- src/reads.py
+++ src/reads.py
@@ -8,26 +8,66 @@ from activity import Activity
 from menu import MenuItem, MenuActivity
 import common
 
+class Finger(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = common.load_image(constants.cursor_filename)
+
+    def update(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.midtop = pos
+
+class Close(pygame.sprite.Sprite):
+   def __init__(self):
+       pygame.sprite.Sprite.__init__(self)
+       self.image, self.rect = common.load_image(constants.icons_folder+'/gartoon/process-stop.png')
+       x = constants.screen_mode[0] - self.image.get_width()
+       y = 0
+       self.rect.topleft = [x,y]
+
 # Actividad 7
 class PoetryActivity(Activity):
     def __init__(self, screen):
         Activity.__init__(self, screen)
+ 
+        self.close_button = Close()
+        self.CloseButton = pygame.sprite.RenderUpdates((self.close_button))
+
+        self.finger = Finger()
+        self.Cursor = pygame.sprite.RenderUpdates((self.finger))
+        self.pos = None
     
     def handle_events(self):
-        event = pygame.event.wait()
-        if event.type == QUIT:
-            self.quit = True
-            return
-        elif event.type == KEYUP:
-            self.changed = False
-            if event.key == K_ESCAPE:
+        for event in [pygame.event.wait()] + pygame.event.get():
+            if event.type == QUIT:
                 self.quit = True
                 return
+            elif event.type == KEYUP:
+                self.changed = False
+                if event.key == K_ESCAPE:
+                    self.quit = True
+                    return
+            elif event.type == MOUSEBUTTONDOWN:
+                if pygame.sprite.collide_rect(self.finger, self.close_button):
+                    self.quit = True
+                    return
+
+        self.pos = pygame.mouse.get_pos()
+        if self.pos != self.mprev:
+            self.changed = True
+
+    def on_change(self):
+        self.Cursor.update()
+        self.CloseButton.draw(self.screen)
+        self.Cursor.draw(self.screen)
+        self.text()
+ 
+        self.mprev = self.pos
 
     def setup_background(self):
         self.background = pygame.image.load(constants.illustration_008)
 
-    def setup(self):
+    def text(self):
         messages = common.load_file(constants.poetry)
         # Title
         font = pygame.font.SysFont("dejavusans", 32)
@@ -54,22 +94,46 @@ class PoetryActivity(Activity):
 class PoetryActivity2(Activity):
     def __init__(self, screen):
         Activity.__init__(self, screen)
+
+        self.close_button = Close()
+        self.CloseButton = pygame.sprite.RenderUpdates((self.close_button))
+
+        self.finger = Finger()
+        self.Cursor = pygame.sprite.RenderUpdates((self.finger))
+        self.pos = None
     
     def handle_events(self):
-        event = pygame.event.wait()
-        if event.type == QUIT:
-            self.quit = True
-            return
-        elif event.type == KEYUP:
-            self.changed = False
-            if event.key == K_ESCAPE:
+        for event in [pygame.event.wait()] + pygame.event.get():
+            if event.type == QUIT:
                 self.quit = True
                 return
+            elif event.type == KEYUP:
+                self.changed = False
+                if event.key == K_ESCAPE:
+                    self.quit = True
+                    return
+            elif event.type == MOUSEBUTTONDOWN:
+                if pygame.sprite.collide_rect(self.finger, self.close_button):
+                    self.quit = True
+                    return
  
+        self.pos = pygame.mouse.get_pos()
+        if self.pos != self.mprev:
+            self.changed = True
+
+    def on_change(self):
+        self.Cursor.update()
+        self.CloseButton.draw(self.screen)
+        self.Cursor.draw(self.screen)
+        self.text()
+
+        self.mprev = self.pos
+
+
     def setup_background(self):
         self.background = pygame.image.load(constants.illustration_012)
         
-    def setup(self):
+    def text(self):
         messages = common.load_file(constants.poetry2)
         
         # Title
