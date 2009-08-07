82011825c883db8abf5a1e15a772fa8ac73ea2d7
Rafael León <leonza99@gmail.com>
2009-08-06 23:38:41 -0430

    Se agrega el texto del juego, el boton para devolver y handle_events

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -13,10 +13,7 @@ import common
 import math
 from icons import Icons
 
-
-
 class Girl(Sprite):
-
     def __init__(self, pos, id, name, exit=(0,0)):
         Sprite.__init__(self)
         self.imagen_normal = constants.images_labyrinth+'/'+name+'.png'
@@ -32,8 +29,28 @@ class Girl(Sprite):
     def update(self):
             if self.img == 1:
                     self.image = pygame.image.load(self.imagen_check)
-        #self.rect.x = pos[0]
-        #self.rect.y = pos[1]
+class Hand(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image_normal = constants.images_cletter+"/all-scroll2.png"
+        self.image_close =  constants.images_cletter+"/grabbing2.png"
+        self.image = pygame.image.load(self.image_normal)
+        self.color = 0
+        self.rect = self.image.get_rect()
+
+    def change_hand(self):
+        if self.color == 0:
+            self.color = 1
+        else:
+            self.color = 0
+
+    def update(self, mover=(0,0)):
+        self.rect.x = mover[0] 
+        self.rect.y = mover[1] 
+        if self.color == 0:
+            self.image = pygame.image.load(self.image_normal)
+        if self.color == 1:
+            self.image = pygame.image.load(self.image_close)
 
 class Ball(Sprite):
     def __init__(self, screen):
@@ -41,7 +58,7 @@ class Ball(Sprite):
         self.image = pygame.image.load(constants.images_labyrinth+\
                 "/ball.png")
         self.rect = self.image.get_rect()
-        self.rect.move_ip((50,50))
+        self.rect.move_ip((50,120))
         self.pos = (0,0)
         self.move_ball = 10
         self.screen = screen
@@ -59,7 +76,6 @@ class Ball(Sprite):
             else:
                 return False
                 
-
     def update(self, pos=(0,0)):
             self.rect.x = pos[0]
             self.rect.y = pos[1]
@@ -124,14 +140,38 @@ class LabyrinthActivity(Activity):
     def setup_background(self):
         self.background = pygame.image.load(constants.illustration_010)
 
+    def informative_text(self):
+        if pygame.font:
+            font = pygame.font.SysFont("dejavusans", 32)
+            font.set_bold(True)
+            text = font.render("El laberinto de la salud", 1, (0, 0, 0))
+            textRect = text.get_rect()
+            textRect.centerx = self.screen.get_rect().centerx
+            textRect.centery = 20
+            self.screen.blit(text, textRect)
+
+            font = pygame.font.SysFont("dejavusans", 20)
+            font.set_bold(False)
+            instructions = \
+                    [u"    Ayuda a Nina a realizar las tareas diarias",
+                    u"a través del laberinto. Utiliza las teclas de",
+                    u"desplazamiento."]
+            y = 40
+            for line in instructions:
+                text = font.render(line, 1,(0, 0, 0))
+                self.screen.blit(text, (20, y))
+                y+=20
+
+
 
     def setup(self):
         pygame.mouse.set_visible( False )
+        self.icons = pygame.sprite.Group()
+        self.icons.add([Icons('stop')])
+        self.hand = Hand()
         self.ball = Ball(self.screen)
         self.Gball = pygame.sprite.Group()
         self.Gball.add([self.ball])
-
-
         self.Girls = pygame.sprite.Group()
         self.Girls.add([Girl((330,85),1,'1',(330, 170)),
                 Girl((280,480),2,'2',(400,473)),
@@ -139,28 +179,36 @@ class LabyrinthActivity(Activity):
                         Girl((495,343),4,'4',(486,387))])
 
         self.GroupSprite = pygame.sprite.OrderedUpdates()
-        self.GroupSprite.add([ self.Gball, self.Girls])
-        self.pos = (100,100)
-        
+        self.GroupSprite.add([ self.icons, self.Gball, self.Girls,
+            self.hand])
+        self.pos = (100,200)
         self.screen.blit(self.background, (0,0))
+        self.informative_text()
         self.GroupSprite.draw(self.screen)
         pygame.display.update()
 
-        pygame.event.set_blocked(MOUSEMOTION)
+        #pygame.event.set_blocked(MOUSEMOTION)
         pygame.key.set_repeat(200, 100)
 
-        exit = False
-
-        while not exit:
-            old_pos = self.ball.pos
+    def handle_events(self): 
+        pygame.event.clear()
+        while True:
             for event in [ pygame.event.wait() ] +  pygame.event.get():
+                pos = pygame.mouse.get_pos()
                 if event.type == QUIT:
                     self.quit = True
-                    exit = True
+                    return
+                elif event.type == MOUSEMOTION:
+                    self.hand.update(pos)
+                elif event.type == MOUSEBUTTONDOWN:
+                  if pygame.sprite.spritecollideany(self.hand,\
+                        self.icons):
+                      self.quit = True
+                      return
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.quit = True
-                        exit = True
+                        return
                     elif event.key == K_UP:
                         self.ball.up_key()
                     elif event.key == K_DOWN:
@@ -173,20 +221,15 @@ class LabyrinthActivity(Activity):
                 selection = pygame.sprite.spritecollideany(self.ball,
                                 self.Girls)
                 if selection:
-                        print self.ball.pos
                         if selection.id == self.ball.counter:
                                 self.ball.counter += 1
                                 selection.img = 1
                                 selection.update()
                                 self.ball.update(selection.exit_pos)
-                                
+              
+              
+            self.screen.blit(self.background, (0,0))
+            self.informative_text()
+            self.GroupSprite.draw(self.screen)
+            pygame.display.update()
 
-
-
-                self.screen.blit(self.background, (0,0))
-                self.GroupSprite.draw(self.screen)
-                pygame.display.update()
-
-        
-        
-        pygame.event.set_allowed(MOUSEMOTION)
