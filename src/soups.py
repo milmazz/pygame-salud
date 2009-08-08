7f064d90b6a11a4bccbead07c653042ce8c64720
Rafael León <leonza99@gmail.com>
2009-08-08 11:09:33 -0430

    Se agregan 5 sopas de letras para cada sopa de letras.

--- src/soups.py
+++ src/soups.py
@@ -4,6 +4,7 @@ import os
 import time
 
 import pygame
+import random 
 from pygame.locals import *
 from pygame.sprite import Sprite
 
@@ -70,8 +71,50 @@ class Palabras(Sprite):
         if palabra == self.palabra:
             self.image = pygame.image.load(self.imagen_raya)
 
-
 class SoupActivity(Activity):
+    def random_position(self):
+        randomSelection = random.randrange(0,5)
+        if randomSelection == 0:
+            self.letrasM =\
+              'ydvgustojjrihrufimlvistadrywttemoIdotcxtianacmunonotactoolfatoat'
+            self.oidoPos  = [32,33,34,35]
+            self.vistaPos = [19,20,21,22,23]
+            self.gustoPos = [3,4,5,6,7]
+            self.tactoPos = [51,52,53,54,55]
+            self.olfatoPos= [56,57,58,59,60,61]
+        if randomSelection == 1:
+            self.letrasM =\
+              'gderoyuiufgtIuhjsuyhdtrotgtyojulvistapofzzxnmkgawetactotgustojho'
+            self.oidoPos  = [4, 12, 20, 28]
+            self.vistaPos = [32, 33, 34, 35, 36]
+            self.gustoPos = [56, 57, 58, 59, 60]
+            self.tactoPos = [50, 51, 52, 53, 54]
+            self.olfatoPos= [23, 31, 39, 47, 55, 63]
+        if randomSelection == 2:
+            self.letrasM =\
+              'gbvpouosudcxswlbswsazxfvttactoaioidxhntsoIdodcotsccbbghanhytgfre'
+            self.oidoPos  = [40, 41, 42, 43]
+            self.vistaPos = [23, 31, 39, 47, 55]
+            self.gustoPos = [0, 8, 16, 24, 32]
+            self.tactoPos = [25, 26, 27, 28, 29]
+            self.olfatoPos= [6, 14, 22, 30, 38, 46]
+        if randomSelection == 3:
+            self.letrasM =\
+              'vistafjoohnjgrfIlnbvcxxdffvtgbhoagustotgttyuiootoujhgbvttactovtb'
+            self.oidoPos  = [7, 15, 23, 31]
+            self.vistaPos = [0, 1, 2, 3, 4]
+            self.gustoPos = [33, 34, 35, 36, 37]
+            self.tactoPos = [56, 57, 58, 59, 60]
+            self.olfatoPos= [8, 16, 24, 32, 40, 48]
+        if randomSelection == 4:
+            self.letrasM =\
+              'olfatoctlasasddaolkjmnhcoIdovghtiujhgbbopoivistamgustodtoiujhggd'
+            self.oidoPos  = [24, 25, 26, 27]
+            self.vistaPos = [43, 44, 45, 46, 47]
+            self.gustoPos = [49, 50, 51, 52, 53]
+            self.tactoPos = [7, 15, 23, 31, 39]
+            self.olfatoPos= [0, 1, 2, 3, 4, 5]
+
     def limpiar_letras_incorrectas(self, lista_coloreadas, palabras):
         #usando los identificadores de las letras
         #verificamos si son las correctas
@@ -88,23 +131,27 @@ class SoupActivity(Activity):
             for i in range(self.tamano_lista):
                 self.lista_ordenada.append(aux[i].id)
             #oido
-            if self.tamano_lista == 4 and self.lista_ordenada == [32,33,34,35]:
+            if self.tamano_lista == 4 and self.lista_ordenada == self.oidoPos:
                 self.check = 2
                 self.palabras.update("oido")
             #vista
-            elif self.tamano_lista == 5 and self.lista_ordenada == [19,20,21,22,23]:
+            elif self.tamano_lista == 5 and self.lista_ordenada ==\
+              self.vistaPos:
                 self.check = 2
                 self.palabras.update("vista")
             #gusto
-            elif self.tamano_lista == 5 and self.lista_ordenada == [3,4,5,6,7]:
+            elif self.tamano_lista == 5 and self.lista_ordenada ==\
+              self.gustoPos:
                 self.check = 2
                 self.palabras.update("gusto")
             #tacto
-            elif self.tamano_lista == 5 and self.lista_ordenada == [51,52,53,54,55]:
+            elif self.tamano_lista == 5 and self.lista_ordenada ==\
+              self.tactoPos:
                 self.check = 2
                 self.palabras.update("tacto")
             #olfato
-            elif self.tamano_lista == 6 and self.lista_ordenada == [56,57,58,59,60,61]:
+            elif self.tamano_lista == 6 and self.lista_ordenada ==\
+              self.olfatoPos:
                 self.check = 2
                 self.palabras.update("olfato")
             elif self.check != 2:
@@ -168,12 +215,12 @@ class SoupActivity(Activity):
         tam_y = 45  #tamano de los cuadros alto
         self.lista = list() #lista de letras que estan coloreadas
         self.informative_text() 
-        #agregar las letras sprite al grupo
-        letrasM = 'ydvgustojjrihrufimlvistadrywttemoIdotcxtianacmunonotactoolfatoat'
+        self.random_position()
+        #letrasM = 'ydvgustojjrihrufimlvistadrywttemoIdotcxtianacmunonotactoolfatoat'
         for i in range(8):
             for j in range(8):
                 self.letras.add([Letras(self.pos_x + tam_x * j, \
-                  pos_y + tam_y * i, letrasM[j + i * 8], j + i * 8)])
+                  pos_y + tam_y * i, self.letrasM[j + i * 8], j + i * 8)])
            
         self.palabras.add([Palabras(110, 370, 'vista'), Palabras(117, 400, 'gusto'), \
           Palabras(108, 430, 'tacto'), Palabras(117, 460, 'oido'), \
@@ -228,6 +275,46 @@ class SoupActivity(Activity):
 
 
 class SoupActivity2(Activity):
+
+    def random_position(self):
+        randomSelection = random.randrange(0,4)
+        if randomSelection == 0:
+            self.letrasM =\
+              'yplantasairenkefdalsueloouyraiumaguaqeioanimalesqntoaudoaluzatan'
+            self.luzPos     =  [57,58,59]
+            self.airePos    =  [8,9,10,11]
+            self.aguaPos    =  [32,33,34,35]
+            self.sueloPos   =  [19,20,21,22,23]
+            self.plantasPos =  [1,2,3,4,5,6,7]
+            self.animalesPos=  [40,41,42,43,44,45,46,47]
+        if randomSelection == 1:
+            self.letrasM =\
+              'airetocalasasddnoluluzhioIeovghmiulhgbbapoovistlaguatodeplantass'
+            self.luzPos     =  [19, 20, 21]
+            self.airePos    =  [0, 1, 2, 3]
+            self.aguaPos    =  [48, 49, 50, 51]
+            self.sueloPos   =  [10, 18, 26, 34, 42]
+            self.plantasPos =  [56, 57, 58, 59, 60, 61, 62]
+            self.animalesPos=  [7, 15, 23, 31, 39, 47, 55, 63]
+        if randomSelection == 2:
+            self.letrasM =\
+              'aguacfrtsuelodcnplantasimjuhgbfcrbnmkloaanimalesuyvcqafeluzaired'
+            self.luzPos     =  [56, 57, 58]
+            self.airePos    =  [59, 60, 61, 62]
+            self.aguaPos    =  [0, 1, 2, 3]
+            self.sueloPos   =  [8, 9, 10, 11, 12]
+            self.plantasPos =  [16, 17, 18, 19, 20, 21, 22]
+            self.animalesPos=  [40, 41, 42, 43, 44, 45, 46, 47]
+        if randomSelection == 3:
+            self.letrasM =\
+              'aairecpanfrfkdlgikrdfcaumfmvmvnaaorkfgtvlluzfbaceunzasswssuelofx'
+            self.luzPos     =  [41, 42, 43]
+            self.airePos    =  [1, 2, 3, 4]
+            self.aguaPos    =  [7, 15, 23, 31]
+            self.sueloPos   =  [57, 58, 59, 60, 61]
+            self.plantasPos =  [6, 14, 22, 30, 38, 46, 54]
+            self.animalesPos=  [0, 8, 16, 24, 32, 40, 48, 56]
+
     def limpiar_letras_incorrectas(self, lista_coloreadas, palabras):
         #usando los identificadores de las letras
         #verificamos si son las correctas
@@ -243,28 +330,34 @@ class SoupActivity2(Activity):
             aux = sorted(lista_coloreadas, self.comparar)
             for i in range(self.tamano_lista):
                 self.lista_ordenada.append(aux[i].id)
+            print self.lista_ordenada
             #luz
-            if self.tamano_lista == 3 and self.lista_ordenada == [57,58,59]:
+            if self.tamano_lista == 3 and self.lista_ordenada == self.luzPos:
                 self.check = 2
                 palabras.update("luz")
             #aire
-            elif self.tamano_lista == 4 and self.lista_ordenada == [8,9,10,11]:
+            elif self.tamano_lista == 4 and self.lista_ordenada ==\
+              self.airePos:
                 self.check = 2
                 palabras.update("aire")
             #agua
-            elif self.tamano_lista == 4 and self.lista_ordenada == [32,33,34,35]:
+            elif self.tamano_lista == 4 and self.lista_ordenada ==\
+              self.aguaPos:
                 self.check = 2
                 palabras.update("agua")
             #suelo
-            elif self.tamano_lista == 5 and self.lista_ordenada == [19,20,21,22,23]:
+            elif self.tamano_lista == 5 and self.lista_ordenada ==\
+              self.sueloPos:
                 self.check = 2
                 palabras.update("suelo")
             #plantas
-            elif self.tamano_lista == 7 and self.lista_ordenada == [1,2,3,4,5,6,7]:
+            elif self.tamano_lista == 7 and self.lista_ordenada ==\
+              self.plantasPos:
                 self.check = 2
                 palabras.update("plantas")
             #animales
-            elif self.tamano_lista == 8 and self.lista_ordenada == [40,41,42,43,44,45,46,47]:
+            elif self.tamano_lista == 8 and self.lista_ordenada ==\
+              self.animalesPos:
                 self.check = 2
                 palabras.update("animales")
             elif self.check != 2:
@@ -327,11 +420,13 @@ class SoupActivity2(Activity):
         #display the informative text in the screen
         self.informative_text()
         #Add the letters to the sprite group
-        letrasM = 'yplantasairenkefdalsueloouyraiumaguaqeioanimalesqntoaudoaluzatan'
+        #generate the random seleccion to the final word position
+        self.random_position()
+        #letrasM = 'yplantasairenkefdalsueloouyraiumaguaqeioanimalesqntoaudoaluzatan'
         for i in range(8):
             for j in range(8):
                 self.letras.add([Letras(self.pos_x + tam_x * j, \
-                  pos_y + tam_y * i, letrasM[j + i * 8], j + i * 8)])
+                  pos_y + tam_y * i, self.letrasM[j + i * 8], j + i * 8)])
            
         self.palabras.add([Palabras(550, 200, 'plantas'), Palabras(550, 250, 'aire'), \
           Palabras(550, 300, 'agua'), Palabras(550, 350, 'luz'), \
