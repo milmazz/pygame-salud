6d5d4c1f8b940c1343bb4124537aa4e510b5197b
Leonel Molina <leonel@telassim.(none)>
2009-08-16 19:17:04 -0430

    Se mejora el rendimiento de la actividad 26

--- src/cooking.py
+++ src/cooking.py
@@ -22,8 +22,7 @@ class Ingredients(Sprite):
         self.name = ingredient
 		
     def update(self, pos):
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.rect.x, self.rect.y = pos
 
 
 class Hand(Sprite):
@@ -45,31 +44,25 @@ class Hand(Sprite):
             self.color = 0
 
     def update(self, mover=(0,0)):
-        if mover[0] - 10 >= 0:
-            self.rect.x = mover[0]
-        if mover[1] - 15 >= 0:
-            self.rect.y = mover[1]
+        self.rect.x, self.rect.y = mover
         if self.color == 0:
             self.image = self.normal
         if self.color == 1:
             self.image = self.close
 
 
-class View():
-    def __init__(self):
+class Cooking(Activity):
+    def __init__(self, screen):
+        Activity.__init__(self, screen)
         self.background = common.load_image(constants.illustration_024)[0]
+        self.selection = None
+        self.instruction_text()
 
     def groupIngredients(self, pos_ingredients):
         ingredients = pygame.sprite.Group()
         for name in pos_ingredients:
             ingredients.add([Ingredients(pos_ingredients[name], name)])
         return ingredients
-
-
-class Cooking(Activity):
-    def __init__(self, screen):
-        Activity.__init__(self, screen)
-        self.selection = None
 
     def instruction_text(self):
         font_title = pygame.font.SysFont("dejavusans", 32)
@@ -82,38 +75,41 @@ class Cooking(Activity):
         y = 5
         text = font_title.render(title, True, (102, 102, 102))
         text_pos = (constants.screen_mode[0]/2.0 - title_width/2.0, y)
-        self.screen.blit(text, text_pos)
+        self.background.blit(text, text_pos)
         y = 20
         line_width, line_height = font_instructions.size(instructions[0])
         for line in instructions:
             text = font_instructions.render(line, True, (102, 102, 102))
             y += line_height
             text_pos = (50, y)
-            self.screen.blit(text, text_pos)
+            self.background.blit(text, text_pos)
 
     def setup(self):
         self.sprites  = pygame.sprite.OrderedUpdates()
         pos_ingredients = {'sugar': (400, 300)} # list of all ingredients and
                                                 # positions on the screen
-        self.view = View() #load static background
         self.hand = Hand() #load hand
         self.icons = pygame.sprite.Group()
         self.icons.add([Icons('stop')])
-        self.ingredients = self.view.groupIngredients(pos_ingredients)
+        self.ingredients = self.groupIngredients(pos_ingredients)
         self.container = pygame.Rect(400, 400, 100, 100)
         self.sprites = pygame.sprite.OrderedUpdates()
         self.sprites.add([self.icons, self.ingredients, self.hand])
         pygame.mouse.set_visible( False ) #hide pointer
         #mouse button is up
         self.button_down = 0
-        self.screen.blit(self.view.background, (0,0))
+        pos = pygame.mouse.get_pos()
+        self.hand.update(pos)
+        self.screen.blit(self.background, (0,0))
         self.sprites.draw(self.screen)
-        self.instruction_text()
         pygame.display.update()
+        pygame.event.clear()
+
 
     def handle_events(self):
         for event in self.get_event():
             pos = pygame.mouse.get_pos()
+            self.hand.update(pos)
             if event.type == QUIT:
                 self.quit = True
                 return
@@ -131,10 +127,10 @@ class Cooking(Activity):
                     self.quit = True
                     return
                 self.hand.change_hand()
-                self.hand.update()
+                self.hand.update(pos)
             if event.type == MOUSEBUTTONUP:
                 self.hand.change_hand()
-                self.hand.update()
+                self.hand.update(pos)
             if event.type == MOUSEBUTTONUP and self.selection:
                 self.button_down = 0
                 ingredients_in_container = \
@@ -151,7 +147,6 @@ class Cooking(Activity):
                 if self.selection:
                     self.selection.color = 1
                     self.button_down = 1
-            self.screen.blit(self.view.background, (0,0))
-            self.instruction_text()
+            self.screen.blit(self.background, (0,0))
             self.sprites.draw(self.screen)
             pygame.display.update()
