68878e9bb9029fc0ded472a80256927e2baa6d5a
Rafael León <leonza99@gmail.com>
2009-08-16 20:50:22 -0430

    Se modifica el laberinto para disminuir cantidad de sprites

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -13,31 +13,18 @@ import common
 import math
 from icons import Icons
 
-class Girl(Sprite):
-    def __init__(self, pos, id, name, exit=(0,0)):
-        Sprite.__init__(self)
-        self.imagen_normal = os.path.join(constants.images_labyrinth, name + '.png')
-        self.imagen_check = os.path.join(constants.images_labyrinth, 
-                                         name + '_check.png')
-        self.image, self.rect = common.load_image(self.imagen_normal)
-        self.rect.move_ip(pos)
-        self.id = id
-        self.img = 0
-        self.exit_pos = exit
-
-    def update(self):
-        if self.img == 1:
-            self.image = pygame.image.load(self.imagen_check)
-
 
 class Hand(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image_normal = constants.images_cletter+"/all-scroll2.png"
-        self.image_close =  constants.images_cletter+"/grabbing2.png"
-        self.image = pygame.image.load(self.image_normal)
+        self.image_normal = os.path.join(constants.data_folder,\
+          'cursors', "hand-open.png")
+        self.image_close = os.path.join(constants.data_folder,\
+          'cursors', "hand-close.png")
+        self.normal, self.rect = common.load_image(self.image_normal)
+        self.close, self.rect = common.load_image(self.image_close)
+        self.image = self.normal 
         self.color = 0
-        self.rect = self.image.get_rect()
 
     def change_hand(self):
         if self.color == 0:
@@ -55,15 +42,14 @@ class Hand(Sprite):
 
 
 class Ball(Sprite):
-    def __init__(self, screen):
+    def __init__(self, screen, pos):
         Sprite.__init__(self)
         path = os.path.join(constants.images_labyrinth, "ball.png")
         self.image, self.rect = common.load_image(path)
-        self.rect.move_ip((50,120))
+        self.rect.move_ip(pos)
         self.pos = (0,0)
-        self.move_ball = 10
+        self.move_ball = 5
         self.screen = screen
-        self.counter = 1
 
     def verify_color_position(self, pos):
         if pos[0] > 0 and pos[0] < constants.screen_mode[0]\
@@ -71,65 +57,62 @@ class Ball(Sprite):
             color = self.screen.get_at(pos)
         else:
                 return False
-        if color[0] >= 220 and color[1] >= 220 and\
-          color[2] >= 150 and color[3] >= 250:
+        if color[0] >= 210 and color[1] >= 144 and\
+          color[2] >= 210 and color[3] >= 250:
             return True
         else:
             return False
 
     def update(self, pos=(0,0)):
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.rect.x, self.rect.y = pos[0], pos[1]
         self.pos = pos
 
     def up_key(self):
-        move = 10
+        move = self.move_ball
         for i in range(1, self.move_ball + 1):
             if (not self.verify_color_position((self.rect.x + 5,
                 self.rect.y - i)) or not\
                 self.verify_color_position((self.rect.x ,\
                 self.rect.y - i)) or not\
                 self.verify_color_position((self.rect.x +\
-                10, self.rect.y - i))) and move == 10:
+                10, self.rect.y - i))) and move == self.move_ball:
                 move = i - 1
         self.update((self.rect.x, self.rect.y - move))
     
     def down_key(self):
-        move = 10
+        move = self.move_ball
         for i in range(1, self.move_ball + 1):
             if (not self.verify_color_position((self.rect.x + 5,\
                 self.rect.y + 10 + i)) or\
                 not self.verify_color_position((self.rect.x,\
                 self.rect.y + 10 + i)) or\
                 not self.verify_color_position((self.rect.x + 10,\
-                self.rect.y + 10 + i))) and move == 10:
+                self.rect.y + 10 + i))) and move == self.move_ball:
                 move = i - 1
         self.update((self.rect.x, self.rect.y + move))
 
     def left_key(self):
-        move = 10
+        move = self.move_ball
         for i in range(1, self.move_ball + 1):
             if (not self.verify_color_position((self.rect.x - i,
                 self.rect.y + 5 )) or\
                 not self.verify_color_position((self.rect.x -i,
                 self.rect.y )) or\
                 not self.verify_color_position((self.rect.x -i,
-                self.rect.y + 10))) and move == 10:
+                self.rect.y + 10))) and move == self.move_ball:
                 move = i - 1
         self.update((self.rect.x - move, self.rect.y))
 
     def right_key(self):
-        move = 10
+        move = self.move_ball
         for i in range(1, self.move_ball + 1):
             if (not self.verify_color_position((self.rect.x + 10 + i,
                 self.rect.y + 5)) or\
                 not self.verify_color_position((self.rect.x + 10
                 + i, self.rect.y)) or\
                 not self.verify_color_position((self.rect.x + 10
-                + i, self.rect.y + 10))) and move == 10:
+                + i, self.rect.y + 10))) and move == self.move_ball:
                 move = i - 1
-        if move < 0:
-            move = 0
         self.update((self.rect.x + move, self.rect.y))
 
  
@@ -152,15 +135,16 @@ class Labyrinth(Activity):
             text = font.render("El laberinto de la salud", 1, (0, 0, 0))
             textRect = text.get_rect()
             textRect.centerx = self.screen.get_rect().centerx
-            textRect.centery = 20
+            textRect.centery = 12
             self.screen.blit(text, textRect)
 
             font = pygame.font.SysFont("dejavusans", 20)
             instructions = \
-                    [u"    Ayuda a Nina a realizar las tareas diarias",
-                    u"a través del laberinto. Utiliza las teclas de",
-                    u"desplazamiento."]
-            y = 40
+                    [u"  Ayuda a Nina a realizar",
+                    u"las tareas diarias a través",
+                    u"del laberinto. Utiliza las",
+                    u"teclas de desplazamiento."]
+            y = 450
             for line in instructions:
                 text = font.render(line, 1,(0, 0, 0))
                 self.screen.blit(text, (20, y))
@@ -168,32 +152,46 @@ class Labyrinth(Activity):
 
     def setup(self):
         pygame.mouse.set_visible( False )
+        path = os.path.join(constants.images_labyrinth, "correct.png")
+        self.checkImage, self.checkImageRect = common.load_image(path)
+        self.counter = 0
+        self.rectangleListEnter = [
+                pygame.Rect(345, 70, 10, 15),
+                pygame.Rect(340, 460, 10, 15),
+                pygame.Rect(300, 370, 10, 15),
+                pygame.Rect(493, 305, 10, 15),
+                pygame.Rect(362, 279, 10, 15)
+                ]
+        self.pointList = [
+                (350, 123),
+                (400, 460),
+                (277, 279),
+                (451, 365)
+                ]
+        self.checkList = [
+                (390, 80),
+                (360, 520),
+                (260, 340),
+                (494, 358),
+                (407, 304),
+                ]
         self.icons = pygame.sprite.Group()
         self.icons.add([Icons('stop')])
         self.hand = Hand()
-        self.ball = Ball(self.screen)
+        self.ball = Ball(self.screen, (200,80))
         self.Gball = pygame.sprite.Group()
         self.Gball.add([self.ball])
-        self.Girls = pygame.sprite.Group()
-        self.Girls.add([Girl((330,85),1,'1',(330, 170)),
-                        Girl((280,480),2,'2',(400,473)),
-                        Girl((120,318),3,'3',(216, 306)),
-                        Girl((495,343),4,'4',(486,387)),
-                        Girl((340,289),5,'5')])
-
         self.GroupSprite = pygame.sprite.OrderedUpdates()
-        self.GroupSprite.add([ self.icons, self.Gball, self.Girls,
-            self.hand])
+        self.GroupSprite.add([ self.icons, self.Gball, self.hand])
         self.pos = (100,200)
         self.screen.blit(self.background, (0,0))
         self.informative_text()
         self.GroupSprite.draw(self.screen)
         pygame.display.update()
-
-        pygame.key.set_repeat(200, 100)
+        pygame.key.set_repeat(50, 50)
 
     def handle_events(self): 
-        for event in self.get_event():
+        for event in [ pygame.event.wait() ] + pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == QUIT:
                 self.quit = True
@@ -219,17 +217,14 @@ class Labyrinth(Activity):
                     self.ball.right_key()
                 self.hand.update((0,0))
 
-            selection = pygame.sprite.spritecollideany(self.ball,
-                            self.Girls)
-            if selection:
-                    if selection.id == self.ball.counter:
-                            self.ball.counter += 1
-                            selection.img = 1
-                            selection.update()
-                            self.ball.update(selection.exit_pos)
-                            self.correct.add(selection)
-
-        if len(self.correct) == 5:
+                if self.counter <= 4 and self.ball.rect.colliderect(self.\
+                  rectangleListEnter[self.counter]):
+                    if self.counter < 4:
+                        self.ball.update(self.pointList[self.counter])
+                    self.background.blit(self.checkImage,\
+                      self.checkList[self.counter])
+                    self.counter += 1
+        if self.counter == 5:
             self.finished_ = True
 
         self.screen.blit(self.background, (0,0))
