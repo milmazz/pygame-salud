c361ab887e06bfb35f10437dc920b855c0439c2b
Rafael León <leonza99@gmail.com>
2009-08-23 16:14:51 -0430

    Se crea la clase para el juego de laberinto dia de limpieza

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -231,3 +231,117 @@ class Labyrinth(Activity):
         self.GroupSprite.draw(self.screen)
         self.informative_text()
         pygame.display.update()
+
+class Labyrinth2Activity(Activity):
+    def __init__(self, screen):
+        Activity.__init__(self, screen)
+        self.correct = set()
+
+    def setup_background(self):
+        self.background = pygame.image.load(constants.illustration_022)
+
+    def setup(self):
+        self.ball = Ball(self.screen)
+        self.Gball = pygame.sprite.Group()
+        self.Gball.add([self.ball])
+
+    def informative_text(self):
+        if pygame.font:
+            font = pygame.font.SysFont("dejavusans", 32)
+            text = font.render(u"Día de limpieza", 1, (0, 0, 0))
+            textRect = text.get_rect()
+            textRect.centerx = self.screen.get_rect().centerx
+            textRect.centery = 20
+            self.screen.blit(text, textRect)
+
+            font = pygame.font.SysFont("dejavusans", 20)
+            instructions = \
+                    [u"Recorre el laberinto y une",
+                    u"los objetos con las palabras"]
+            y = 60
+            for line in instructions:
+                text = font.render(line, 1,(0, 0, 0))
+                self.screen.blit(text, (20, y))
+                y+=20
+
+    def setup(self):
+        pygame.mouse.set_visible( False )
+        path = os.path.join(constants.images_labyrinth, "correct.png")
+        self.checkImage, self.checkImageRect = common.load_image(path)
+        self.counter = 0
+        self.rectangleListEnter = [
+                pygame.Rect(212, 342, 50, 25),
+                pygame.Rect(354, 407, 3, 20),
+                pygame.Rect(251, 506, 1, 25),
+                pygame.Rect(600, 320, 12, 6),
+                pygame.Rect(470, 522, 7, 17)
+                ]
+        self.pointList = [
+                (130, 390),
+                (130, 564),
+                (534, 201),
+                (642, 489)
+                ]
+        self.checkList = [
+                (250, 305),
+                (400, 391),
+                (290, 535),
+                (607, 279),
+                (521, 540),
+                ]
+        self.icons = pygame.sprite.Group()
+        self.icons.add([Icons('stop')])
+        self.hand = Hand()
+        self.ball = Ball(self.screen, (400,250))
+        self.Gball = pygame.sprite.Group()
+        self.Gball.add([self.ball])
+        self.GroupSprite = pygame.sprite.OrderedUpdates()
+        self.GroupSprite.add([ self.icons, self.Gball, self.hand])
+        self.pos = (100,200)
+        self.screen.blit(self.background, (0,0))
+        self.informative_text()
+        self.GroupSprite.draw(self.screen)
+        pygame.display.update()
+        pygame.key.set_repeat(50, 50)
+
+    def handle_events(self): 
+        for event in [ pygame.event.wait() ] + pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == QUIT:
+                self.quit = True
+                return
+            elif event.type == MOUSEMOTION:
+                self.hand.update(pos)
+            elif event.type == MOUSEBUTTONDOWN:
+              if pygame.sprite.spritecollideany(self.hand,\
+                    self.icons):
+                  self.quit = True
+                  return
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.quit = True
+                    return
+                elif event.key == K_UP:
+                    self.ball.up_key()
+                elif event.key == K_DOWN:
+                    self.ball.down_key()
+                elif event.key == K_LEFT:
+                    self.ball.left_key()
+                elif event.key == K_RIGHT:
+                    self.ball.right_key()
+                self.hand.update((0,0))
+
+                if self.counter <= 4 and self.ball.rect.colliderect(self.\
+                  rectangleListEnter[self.counter]):
+                    if self.counter < 4:
+                        self.ball.update(self.pointList[self.counter])
+                    self.background.blit(self.checkImage,\
+                      self.checkList[self.counter])
+                    self.counter += 1
+        if self.counter == 5:
+            self.finished_ = True
+
+        self.screen.blit(self.background, (0,0))
+        self.GroupSprite.draw(self.screen)
+        self.informative_text()
+        pygame.display.update()
