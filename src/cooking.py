829e552867d07a59b53d4094bff8429dbfbfff40
Leonel Molina <leonel@telassim.(none)>
2009-08-24 18:01:48 -0430

    Nueva version de la actividad 26
    * Se reescribe totalmente la actividad
    * Se agregan los nuevos ingredientes
    * Se agrega retroalimentacion
    * Los ingredientes se posicionan aleatoriamente sobre la mesa

--- src/cooking.py
+++ src/cooking.py
@@ -4,25 +4,28 @@
 import os
 import sys
 import pygame
+import random
 from pygame.sprite import Sprite
 from pygame.locals import *
 
 import constants
 from activity import Activity
 import common
-from icons import Icons
+from icons import *
 
-class Ingredients(Sprite):
-    def __init__(self, pos, ingredient):
+class Ingredient(Sprite):
+    def __init__(self, name, pos=(0,0)):
         Sprite.__init__(self)
         image_name = os.path.join(constants.data_folder, "cooking",
-                                  ingredient  + ".png")
+                                  name + ".png")
         self.image, self.rect = common.load_image(image_name)
+        self.size_x, self.size_y = self.image.get_size()
         self.rect.move_ip(pos)
-        self.name = ingredient
-		
+        self.orig = pos
+        self.name = name
+	
     def update(self, pos):
-        self.rect.x, self.rect.y = pos
+        self.rect.topleft = pos
 
 
 class Hand(Sprite):
@@ -44,7 +47,7 @@ class Hand(Sprite):
             self.color = 0
 
     def update(self, mover=(0,0)):
-        self.rect.x, self.rect.y = mover
+        self.rect.topleft = mover
         if self.color == 0:
             self.image = self.normal
         if self.color == 1:
@@ -55,23 +58,22 @@ class Cooking(Activity):
     def __init__(self, screen):
         Activity.__init__(self, screen)
         self.background = common.load_image(constants.illustration_024)[0]
-        self.selection = None
         self.instruction_text()
 
-    def groupIngredients(self, pos_ingredients):
+    def groupingredients(self, pos_ingredients):
         ingredients = pygame.sprite.Group()
         for name in pos_ingredients:
-            ingredients.add([Ingredients(pos_ingredients[name], name)])
+            ingredients.add([Ingredient(name, pos_ingredients[name])])
         return ingredients
 
     def instruction_text(self):
         font_title = pygame.font.SysFont("dejavusans", 32)
         font_instructions = pygame.font.SysFont("dejavusans", 20)
-        title = u"¡A cocinar!"
+        title = u"¡De compras!"
         title_width, title_height = font_title.size(title)
-        instructions = [u"Vamos a hacer una torta ...",
-                        u"Observa los ingredientes que se necesitan",
-                        u"y arrástralos hasta la mesa."]
+        instructions = [u"Coloca en cada carrito de mercado los", \
+                u" alimentos que corresponden.", u"Arrastra el" \
+                u" alimento al carrito respectivo."]
         y = 5
         text = font_title.render(title, True, (102, 102, 102))
         text_pos = (constants.screen_mode[0]/2.0 - title_width/2.0, y)
@@ -81,40 +83,39 @@ class Cooking(Activity):
         for line in instructions:
             text = font_instructions.render(line, True, (102, 102, 102))
             y += line_height
-            text_pos = (50, y)
+            text_pos = (35, y)
             self.background.blit(text, text_pos)
 
     def setup(self):
+        self.selection = None
         self.sprites  = pygame.sprite.OrderedUpdates()
-        pos_ingredients = {'sugar': (400, 300)} # list of all ingredients and
-                                                # positions on the screen
+        self.pos_ingredients = {'butter': (52, 162), 'eggs': (295, 238), \
+                'flour': (57, 347), 'ketchup': (295, 450), 'mayonnaise': (0,
+                    0), 'milk': (0, 0), 'pepper': (0, 0), 'sugar': (0, 0)}
+        # list of all food and positions on the screen
+        self.checked_ingredients = 0
         self.hand = Hand() #load hand
         self.icons = pygame.sprite.Group()
         self.icons.add([Icons('stop')])
-        self.ingredients = self.groupIngredients(pos_ingredients)
-        self.container = pygame.Rect(400, 400, 100, 100)
+        self.checked = pygame.sprite.Group()
+        self.ingredients = self.groupingredients(self.pos_ingredients)
+        self.container = pygame.Rect(370, 315, 225, 65)
         self.sprites = pygame.sprite.OrderedUpdates()
         self.sprites.add([self.icons, self.ingredients, self.hand])
         pygame.mouse.set_visible( False ) #hide pointer
-        #mouse button is up
         self.button_down = 0
         pos = pygame.mouse.get_pos()
         self.hand.update(pos)
-        self.screen.blit(self.background, (0,0))
         self.sprites.draw(self.screen)
-        pygame.display.update()
         pygame.event.clear()
-
 
     def handle_events(self):
         for event in self.get_event():
             pos = pygame.mouse.get_pos()
-            self.hand.update(pos)
             if event.type == QUIT:
                 self.quit = True
                 return
             elif event.type == KEYUP:
-                self.changed = False
                 if event.key == K_ESCAPE:
                     self.quit = True
                     return
@@ -133,20 +134,42 @@ class Cooking(Activity):
                 self.hand.update(pos)
             if event.type == MOUSEBUTTONUP and self.selection:
                 self.button_down = 0
-                ingredients_in_container = \
+                ingredient_in_container = \
                         self.container.colliderect(self.selection.rect)
-                if ingredients_in_container:
-#                        print "Ingrediente: " + selection.name + " esta adentro"
-                    pass
+                if ingredient_in_container:
+#                    self.selection.update(pos)
+                    self.selection.rect.topleft = \
+                            (self.container[0] \
+                            + random.randrange(0, \
+                            self.container[2]\
+                            - self.selection.size_x), \
+                            self.container[1]\
+                            - self.selection.size_y + 20\
+                            + random.randrange(0, \
+                            self.container[3] - 20))
+#                            - self.selection.size_y))
+                    self.selection.rect.size = (0, 0)
+                    self.selection.kill()
+                    self.selection.add(self.sprites)
+                    self.checked_ingredients += 1
+                    if self.checked_ingredients >= 4:
+                        self.sprites.add([Check(self.container.center)])
+                        self.finished_ = True
                 else:
-                    self.selection.color = 0
-                    self.selection.update(pos)
+                    self.selection.update((self.selection.orig))
             if event.type == MOUSEBUTTONDOWN:
-                self.selection = pygame.sprite.spritecollideany(self.hand, \
-                        self.ingredients)
+                self.selection = pygame.sprite.Group()
+                if pygame.sprite.spritecollideany(self.hand, \
+                        self.ingredients):
+                    self.selection = pygame.sprite.spritecollideany(self.hand, \
+                            self.ingredients)
                 if self.selection:
-                    self.selection.color = 1
                     self.button_down = 1
+                    self.selection.remove(self.sprites)
+                    self.selection.add(self.sprites)
+            self.hand.remove(self.sprites)
+            self.hand.add(self.sprites)
+            pygame.draw.rect(self.background, (0, 0, 0), self.container)
             self.screen.blit(self.background, (0,0))
             self.sprites.draw(self.screen)
             pygame.display.update()
