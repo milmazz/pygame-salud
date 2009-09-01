3b1796dacb72cda5be877f346bc8e9f7bfdc3bf1
Milton Mazzarri <milmazz@gmail.com>
2009-09-01 01:39:03 -0430

    Refactorización inicial de las actividades de Seleccionar.
    TODO: Falta refactorizar la clase Select4Activity.

--- src/select.py
+++ src/select.py
@@ -40,43 +40,9 @@ class Hand(Sprite):
             self.image = pygame.image.load(self.image_close)
 
 
-class SelectActivity(Activity):
+class SelectBase(Activity):
     def __init__(self, screen):
         Activity.__init__(self, screen)
-
-    def setup_background(self):
-        self.background = common.load_image(constants.illustration_011)[0]
-        self.instruction_text()
-
-    def instruction_text(self):
-        if pygame.font:
-            font = pygame.font.SysFont(constants.font_title[0], constants.font_title[1])
-            title = unicode("Una boca bien sana", 'utf-8')
-            text = font.render(title, True, (0, 0, 0))
-            textRect = text.get_rect()
-            textRect.centerx = self.screen.get_rect().centerx
-            textRect.centery = 20
-            self.background.blit(text, textRect)
-
-            messages = ["  Selecciona aquellos objetos",
-            "que son necesarios para mantener tu boca sana."]
-            
-            y = font.get_linesize()
-            font = pygame.font.SysFont(constants.font_default[0], constants.font_default[1])
-            font_height = font.get_linesize()
-
-            for message in messages:
-                message = unicode(message, 'utf-8')
-                text = font.render(message, True, (122, 122, 122))
-                textRect = text.get_rect()
-                textRect.centerx = self.screen.get_rect().centerx
-                y += font_height 
-                textRect.centery = y
-                self.background.blit(text, textRect)
-
-    def setup(self):
-        """Turn off the mouse pointer"""
-        pygame.mouse.set_visible( False )
         """change the mouse pointer by a hand"""
         self.button_down = 0
         self.rectList = [ 
@@ -92,6 +58,54 @@ class SelectActivity(Activity):
         self.checkImage, self.checkRect = common.load_image\
           (image_check)
         self.hand = Hand()
+        self.tex_info = {
+            'title': ["Una boca bien sana", None],
+            'instructions' : [["Selecciona aquellos objetos", \
+                            "que son necesarios para mantener tu boca sana"], None]
+        }
+
+    def setup_background(self):
+        self.background, self.rect = common.load_image(constants.illustration_011)
+
+    def instruction_text(self, info):
+        if pygame.font:
+            title = info['title'][0]
+            title_pos = info['title'][1]
+            messages = info['instructions'][0]
+            font = pygame.font.SysFont(constants.font_title[0], constants.font_title[1])
+            text = font.render(title, True, (0, 0, 0))
+            # Centering the text
+            if title_pos == None:
+                title_pos = text.get_rect()
+                title_pos.centerx = self.screen.get_rect().centerx
+                title_pos.centery = 20
+            self.background.blit(text, title_pos)
+            
+            if info['instructions'][1] == None:
+                y = font.get_linesize()
+            else:
+                y = info['instructions'][1]
+
+            font = pygame.font.SysFont(constants.font_default[0], constants.font_default[1])
+            font_height = font.get_linesize()
+
+            for message in messages:
+                y += font_height
+                message = unicode(message, 'utf-8')
+                text = font.render(message, True, (122, 122, 122))
+                message_pos = text.get_rect()
+                # Centering text instructions
+                if info['title'][1] == None:
+                    message_pos.centerx = self.screen.get_rect().centerx
+                    message_pos.centery = y
+                else:
+                    message_pos.topleft = (info['title'][1][0], y)
+                self.background.blit(text, message_pos)
+
+    def setup(self):
+        """Turn off the mouse pointer"""
+        pygame.mouse.set_visible( False )
+        self.instruction_text(self.tex_info)
         self.sprites.add([self.icons, self.hand])
         pos = pygame.mouse.get_pos()
         self.sprites.draw(self.screen)
@@ -134,192 +148,50 @@ class SelectActivity(Activity):
         if len(self.rectList) == 0:
             self.finished_ = True
 
-class Select2Activity(Activity):
+class SelectActivity(SelectBase):
     def __init__(self, screen):
-        Activity.__init__(self, screen)
+        SelectBase.__init__(self, screen)
 
-    def setup_background(self):
-        self.background = common.load_image(constants.illustration_016)[0]
-        self.instruction_text()
 
-    def instruction_text(self):
-        if pygame.font:
-            font = pygame.font.SysFont(constants.font_title[0], constants.font_title[1])
-            title = unicode("La salud en nuestras playas", 'utf-8')
-            text = font.render(title, True, (0, 0, 0))
-            textRect = text.get_rect()
-            textRect.topleft = (30, 10)
-            self.background.blit(text, textRect)
-
-            messages = ["¿Quién está haciendo bien las cosas?",
-            "Selecciona las actividades", "correctas en el dibujo."]
-            
-            y = font.get_linesize()
-            font = pygame.font.SysFont(constants.font_default[0], constants.font_default[1])
-            font_height = font.get_linesize()
-
-            for message in messages:
-                message = unicode(message, 'utf-8')
-                text = font.render(message, True, (122, 122, 122))
-                textRect = text.get_rect()
-                y += font_height 
-                textRect.topleft = (30, y)
-                self.background.blit(text, textRect)
-
-    def setup(self):
-        """Turn off the mouse pointer"""
-        pygame.mouse.set_visible( False )
-        """change the mouse pointer by a hand"""
-        self.button_down = 0
-        self.rectList = [ 
+class Select2Activity(SelectBase):
+    def __init__(self, screen):
+        SelectBase.__init__(self, screen)
+        self.rectList = [
                 pygame.Rect(236, 235, 71, 39),
                 pygame.Rect(238, 326, 84, 162),
                 pygame.Rect(410, 273, 88, 77),
                 ]
-        self.sprites = pygame.sprite.OrderedUpdates()
-        self.icons = pygame.sprite.Group()
-        self.icons.add([Icons('stop')])
-        image_check = image_normal = os.path.join(constants.icons_folder,\
-                "check.png")
-        self.checkImage, self.checkRect = common.load_image\
-          (image_check)
-        self.hand = Hand()
-        self.sprites.add([self.icons, self.hand])
-        pos = pygame.mouse.get_pos()
-        self.sprites.draw(self.screen)
-
-    def handle_events(self):
-        verify_rect = None
-        for event in self.get_event():
-            pos = pygame.mouse.get_pos()
-            if event.type == QUIT:
-                self.quit = True
-                return
-            elif event.type == MOUSEMOTION:
-                    self.hand.update(pos)
-
-            elif event.type == KEYUP:
-                if event.key == K_ESCAPE:
-                    self.quit = True
-                    return
-            elif event.type == MOUSEBUTTONDOWN:
-                if pygame.sprite.spritecollideany\
-                  (self.hand, self.icons):
-                    self.quit = True
-                    return
-                verify_rect = self.hand.rect.collidelistall(self.rectList)
-                if verify_rect:
-                    """If the selection is a valid object,
-                    delete the object from the list"""
-                    objectSelected = self.rectList[verify_rect[0]]
-                    del self.rectList[verify_rect[0]]
-                    """draw the check image in the center
-                    of the rect selected"""
-                    self.background.blit(self.checkImage,\
-                     (objectSelected.centerx - self.checkRect.centerx\
-                     , objectSelected.centery - self.checkRect.centery ))
-
-        self.screen.blit(self.background, (0,0))
-        self.sprites.draw(self.screen)
-        pygame.display.update()
-
-        if len(self.rectList) == 0:
-            self.finished_ = True
-
-class Select3Activity(Activity):
-    def __init__(self, screen):
-        Activity.__init__(self, screen)
+        self.tex_info = {
+            'title': ["La salud en nuestras playas", (30, 10)],
+            'instructions' : [["¿Quién está haciendo bien las cosas?", \
+                            "Selecciona las actividades",
+                            "correctas en el dibujo."], None]
+        }
 
     def setup_background(self):
-        self.background = common.load_image(constants.illustration_015)[0]
-        self.instruction_text()
+        self.background, self.rect = common.load_image(constants.illustration_016) 
 
-    def instruction_text(self):
-        if pygame.font:
-            font = pygame.font.SysFont(constants.font_title[0], constants.font_title[1])
-            title = unicode("Somos diferentes", 'utf-8')
-            text = font.render(title, True, (0, 0, 0))
-            textRect = text.get_rect()
-            textRect.centerx = self.screen.get_rect().centerx
-            textRect.centery = 20
-            self.background.blit(text, textRect)
 
-            messages = ['En cada uno de las ilustraciones, selecciona según se indica. Ilustración A, el más alto;',
-            'ilustración B, la más flaca; ilustración C, el más blanco; ilustración D, el más fuerte.']
-            
-            y = 252
-            font = pygame.font.SysFont(constants.font_default[0], 25)
-            font_height = font.get_linesize()
-
-            for message in messages:
-                message = unicode(message, 'utf-8')
-                text = font.render(message, True, (0, 0, 0))
-                textRect = text.get_rect()
-                textRect.centerx = self.screen.get_rect().centerx
-                y += font_height 
-                textRect.centery = y
-                self.background.blit(text, textRect)
-
-    def setup(self):
-        """Turn off the mouse pointer"""
-        pygame.mouse.set_visible( False )
-        """change the mouse pointer by a hand"""
-        self.button_down = 0
+class Select3Activity(SelectBase):
+    def __init__(self, screen):
+        SelectBase.__init__(self, screen)
         self.rectList = [ 
                 pygame.Rect(160, 62, 70, 195),
                 pygame.Rect(595, 59, 49, 154),
                 pygame.Rect(141, 339, 59, 199),
                 pygame.Rect(472, 317, 69, 257),
                 ]
-        self.sprites = pygame.sprite.OrderedUpdates()
-        self.icons = pygame.sprite.Group()
-        self.icons.add([Icons('stop')])
-        image_check = image_normal = os.path.join(constants.icons_folder,\
-                "check.png")
-        self.checkImage, self.checkRect = common.load_image\
-          (image_check)
-        self.hand = Hand()
-        self.sprites.add([self.icons, self.hand])
-        pos = pygame.mouse.get_pos()
-        self.sprites.draw(self.screen)
+        self.tex_info = {
+            'title': ["Somos diferentes", None],
+            'instructions' : [["En cada uno de las ilustraciones, " + \
+                    "selecciona según se indica. Ilustración A, el más alto;",
+                    "ilustración B, la más flaca; ilustración C, el" + \
+                    "más blanco; ilustración D, el más fuerte."], 252]
+        }
 
-    def handle_events(self):
-        verify_rect = None
-        for event in self.get_event():
-            pos = pygame.mouse.get_pos()
-            if event.type == QUIT:
-                self.quit = True
-                return
-            elif event.type == MOUSEMOTION:
-                    self.hand.update(pos)
+    def setup_background(self):
+        self.background, self.rect = common.load_image(constants.illustration_015)
 
-            elif event.type == KEYUP:
-                if event.key == K_ESCAPE:
-                    self.quit = True
-                    return
-            elif event.type == MOUSEBUTTONDOWN:
-                if pygame.sprite.spritecollideany\
-                  (self.hand, self.icons):
-                    self.quit = True
-                    return
-                verify_rect = self.hand.rect.collidelistall(self.rectList)
-                if verify_rect:
-                    """If the selection is a valid object,
-                    delete the object from the list"""
-                    objectSelected = self.rectList[verify_rect[0]]
-                    del self.rectList[verify_rect[0]]
-                    """draw the check image in the center
-                    of the rect selected"""
-                    self.background.blit(self.checkImage,\
-                     (objectSelected.centerx - self.checkRect.centerx,\
-                     objectSelected.centery - self.checkRect.centery))
-
-        self.screen.blit(self.background, (0,0))
-        self.sprites.draw(self.screen)
-        pygame.display.update()
-
-        if len(self.rectList) == 0:
-            self.finished_ = True
 
 class Select4Activity(Activity):
     def __init__(self, screen):
@@ -412,4 +284,3 @@ class Select4Activity(Activity):
 
         if len(self.rectList) == 0:
             self.finished_ = True
-
