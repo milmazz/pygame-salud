747d7924c76c692f21aa7ce1bcdb89fb9d2505cb
Milton Mazzarri <milmazz@gmail.com>
2009-09-10 21:27:20 -0430

    Inclusión de la clase de botones de navegación (siguiente y previo).

--- src/icons.py
+++ src/icons.py
@@ -4,13 +4,12 @@
 import os
 import pygame
 from pygame.locals import *
-from pygame.sprite import Sprite
 import constants
 import common
 
-class Icons(Sprite):
+class Icons(pygame.sprite.Sprite):
    def __init__(self, id):
-       Sprite.__init__(self)
+       pygame.sprite.Sprite.__init__(self)
        path = os.path.join(constants.icons_folder, "gartoon",
                            "process-stop.png")
        self.image, self.rect = common.load_image(path)
@@ -20,9 +19,9 @@ class Icons(Sprite):
        self.id = id
 
 
-class Check(Sprite):
+class Check(pygame.sprite.Sprite):
     def __init__(self, pos=None, zoom=0, size=None):
-        Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
         self.path_check = os.path.join(constants.data_folder, \
                 'icons', "check.png")
         self.image, self.rect = common.load_image(self.path_check)
@@ -35,4 +34,30 @@ class Check(Sprite):
             pos = map(lambda x: x/2.0, constants.screen_mode)
         self.rect.centerx = pos[0]
         self.rect.centery = pos[1]
+
+class Navigation(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.path_next = os.path.join(constants.icons_folder, 'gartoon',
+                                    "next.png")
+        self.path_prev = os.path.join(constants.icons_folder, 'gartoon',
+                                    "prev.png")
+        self.next, self.next_rect = common.load_image(self.path_next)
+        self.prev, self.prev_rect = common.load_image(self.path_prev)
+        self.next_rect.move_ip(constants.screen_mode[0] - \
+                self.next_rect[2] - 5, constants.screen_mode[1] - \
+                self.next_rect[3] - 5)
+        self.prev_rect.move_ip(5, constants.screen_mode[1] - \
+                self.prev_rect[3] - 5)
+        self.image = self.next
+        self.rect = self.next_rect
+
+    def update(self):
+        if self.image == self.next:
+            self.image = self.prev
+            self.rect = self.prev_rect
+        else:
+            self.image = self.next
+            self.rect = self.next_rect
+
 
